f28b0865ba8a3dbb1bb0c3a2939e3ff6c40d7f0e
Prince Jhabak <princejhabak11.pj@gmail.comgit config --global user.namegit config --global user.email>
2017-01-18 20:47:00 +0530

    first commit

---
+++ prince.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int x,y,sum;
+printf("enter first number =");
+scanf("%d",&x);
+printf("enter second number =");
+scanf("%d",&y);
+sum=x+y;
+printf("sum is %d",sum);
+}
+ 
